7831caa44bf57f1106851ec09e47d66a3564c53b
Yaroslav <Yaroslavstepura@gmail.com>
2021-03-10 22:16:11 +0200

    homework1

--- HW7/js/task-5.js
+++
@@ -1,6 +0,0 @@
-const inputNameRef = document.querySelector('#name-input');
-const nameRef = document.querySelector('#name-output');
-
-inputNameRef.addEventListener('input', () => {
-    nameRef.textContent = inputNameRef.value ? inputNameRef.value : 'незнайомець';
-});
