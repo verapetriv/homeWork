42de9f4cd99511cb32815ac4dc2c9e052171b95c
Yaroslav <Yaroslavstepura@gmail.com>
2021-03-10 22:13:09 +0200

    reset

--- HW1/js/task-2.js
+++
@@ -1,5 +0,0 @@
-const total = 100;
-const ordered = 130;
-
-ordered > total ? console.log("На складе недостаточно товаров!")
-: console.log("Заказ оформлен, с вами свяжется менеджер");
--- HW7/js/task-7.js
+++
@@ -1,6 +0,0 @@
-const fontSizeControlRef = document.querySelector('#font-size-control');
-const textRef = document.querySelector('#text');
-
-fontSizeControlRef.addEventListener('input', (event) => {
-    textRef.style.fontSize = `${fontSizeControlRef.value / 2}px`;
-});
